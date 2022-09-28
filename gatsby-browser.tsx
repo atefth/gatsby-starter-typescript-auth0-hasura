83feb8646deca708f8d68c2ec8ae4a8d316aef89
Atef Haque <atefth@gmail.com>
2022-09-28 06:55:01 +0600

    Update gatsby-browser.tsx

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -11,7 +11,7 @@ export const wrapRootElement = ({ element }) => {
     <Auth0Provider
       domain={process.env.GATSBY_AUTH0_DOMAIN}
       clientId={process.env.GATSBY_AUTH0_CLIENT_ID}
-      redirectUri={process.env.GATSBY_AUTH0_REDIRECT_URI}
+      redirectUri={window.location.origin}
       audience={process.env.GATSBY_AUTH0_AUDIENCE}
       responseType="token id_token"
       scope="openid profile email offline_access"
